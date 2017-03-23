9b19b839f282c2ece8a7be8077f986031a33ad15
gsj0001 <gsj0001@tigermail.auburn.edu>
2017-03-23 16:31:20 -0500

    Green Light |

--- softwareprocess/dispatch.py
+++ softwareprocess/dispatch.py
@@ -11,7 +11,7 @@ def dispatch(values=None):
 
     #Perform designated function
     if(values['op'] == 'adjust'):
-        if not ('observation' in values) or not ('height' in values) or not ('pressure' in values) or not ('horizon' in values) or not ('temperature' in values) :
+        if ( not 'observation' in values) or  ( not 'height' in values) or ( not 'pressure' in values) or ( not 'horizon' in values) or ( not 'temperature' in values) :
             return {'error' : 'mandatory information is missing'}
 
         return values    #<-------------- replace this with your implementation
