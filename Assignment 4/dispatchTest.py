d5f3cd8d1b9b97f2a7e22aecff4dcd45234cd7e4
gsj0001 <gsj0001@tigermail.auburn.edu>
2017-03-22 21:05:55 -0500

    Reorganizing the test environment; Only testing for Assignment 4

---
+++ Assignment 4/dispatchTest.py
@@ -0,0 +1,20 @@
+import unittest
+
+import dispatch as SM
+
+
+class dispatchTest(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    # Some Sad paths, that came with the base code
+    def test100_910_NoValuesGiven(self):
+        self.assertEquals(SM.dispatch() , {'error': 'parameter is missing'})
+    def test100_911_IsNotADictionary(self):
+        self.assertEquals(SM.dispatch("123123"), {'error': 'parameter is not a dictionary'})
+    def test100_912_NotOp(self):
+        self.assertEquals(SM.dispatch( {'notop' : 'doesnt matter'}) , 'no op  is specified')
