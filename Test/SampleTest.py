3b247018573f550be001b6bedfbcd48e0ddc0585
gsj0001 <gsj0001@tigermail.auburn.edu>
2017-03-22 21:19:47 -0500

    Green Light |

--- Test/SampleTest.py
+++ Test/SampleTest.py
@@ -1,17 +1,17 @@
-# import math
-# import unittest
+ import math
+ import unittest
+
+import Sample as SM
 #
-# import Sample as SM
 #
+ class SampleTest(unittest.TestCase):
+
+    def setUp(self):
+         self.nominalN  = 4
+         self.nominalT = 1.4398
 #
-# class SampleTest(unittest.TestCase):
-#
-#     def setUp(self):
-#         self.nominalN  = 4
-#         self.nominalT = 1.4398
-#
-#     def tearDown(self):
-#         pass
+     def tearDown(self):
+         pass
 # # -----------------------------------------------------------------------
 # # ---- Acceptance Tests
 # # 100 constructor
