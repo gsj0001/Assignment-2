e7168d39e11dea73b09cd98a57f7a4ac4931316d
gsj0001 <gsj0001@tigermail.auburn.edu>
2017-04-07 19:47:02 -0500

    Red Light |

--- softwareprocess/Test/predictTest.py
+++ softwareprocess/Test/predictTest.py
@@ -6,3 +6,5 @@ class predictTest(unittest.TestCase):
 
     def test1000_MandatoryInformationMissing(self):
         self.assertEquals(SM.predict({'op':'predict'}), {'error':'mandatory information is missing', 'op':'predict'})
+
+    def test1001_
