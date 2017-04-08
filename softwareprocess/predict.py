8806fb00f0e1069858e0ac2d5cdcf6c627fa78f4
gsj0001 <gsj0001@tigermail.auburn.edu>
2017-04-08 17:29:56 -0500

    Green Light |

--- softwareprocess/predict.py
+++ softwareprocess/predict.py
@@ -21,6 +21,8 @@ def predict(values=None):
 
     #Check values within date
     dateValues = values['date'].split('-')
+    for s in dateValues:
+        print(s)
     if(dateValues[0] > __CURRENT_YEAR__ or dateValues[1] > 12 or dateValues[2] > 31):
         values['error'] = 'invalid date'
         return values
