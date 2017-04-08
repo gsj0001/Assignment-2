8ecfa6b0a2e2fff0583a9947038803f72c3a56d2
gsj0001 <gsj0001@tigermail.auburn.edu>
2017-04-07 19:42:17 -0500

    Green Light |

--- softwareprocess/predict.py
+++ softwareprocess/predict.py
@@ -1,3 +1,4 @@
 def predict(values=None):
-    if(values == None or )
-    return
+    if(values == None or not 'body' in values or not 'date' in values or not 'time' in values):
+        return {'error':'mandatory information is missing', 'op':'predict'}
+    return  values
