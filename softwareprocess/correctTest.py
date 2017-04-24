ecef861514a50fe875a97112c66f8dc07906a075
gsj0001 <gsj0001@tigermail.auburn.edu>
2017-04-24 15:20:02 -0500

    Green Light |

--- softwareprocess/correctTest.py
+++ softwareprocess/correctTest.py
@@ -3,3 +3,5 @@ import unittest
 import softwareprocess.correct as SM
 
 class correctTest(unittest.TestCase):
+    def test1000_MandatoryInformationMissing(self):
+        self.assertEquals(SM.predict({'op':'correct'}), {'error':'mandatory information is missing', 'op':'correct'})
