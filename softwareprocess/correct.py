44852f53fcc99ba82ad48f052dbd3e2e392a082e
gsj0001 <gsj0001@tigermail.auburn.edu>
2017-04-24 15:21:31 -0500

    Green Light |

--- softwareprocess/correct.py
+++ softwareprocess/correct.py
@@ -3,7 +3,7 @@
 def correct(values=None):
 
     if(values == None or not 'lat' in values or not 'long' in values or not 'altitude' in values or not 'assumedLat' in values or not 'assumedLong' in values):
-        values['error'] = 'mandatory values missing'
+        values['error'] = 'mandatory information is missing'
         return values
 
 
