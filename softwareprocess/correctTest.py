2d886a55c99fc32de9cf01ee528f335badab2fbb
gsj0001 <gsj0001@tigermail.auburn.edu>
2017-04-27 17:47:16 -0500

    Green Light |

--- softwareprocess/correctTest.py
+++ softwareprocess/correctTest.py
@@ -17,6 +17,6 @@ class correctTest(unittest.TestCase):
                                      {'op': 'correct', 'lat': '16d32.3', 'long': '95d41.6', 'altitude': '13d42.3',
                                       'assumedLat': '-153d38.4', 'assumedLong': '74d35.3','error':'invalid assumedLat'})
     def test2000_ValueCheck(self):
-        self.assertEquals(SM.correct({'op':'correct', 'lat': '16d32.3', 'long':'95d41.6','altitude':'13d42.3','assumedLat':'-153d38.4','assumedLong':'74d35.3'}),
+        self.assertEquals(SM.correct({'op':'correct', 'lat': '16d32.3', 'long':'95d41.6','altitude':'13d42.3','assumedLat':'-53d38.4','assumedLong':'74d35.3'}),
                           {'op': 'correct', 'lat': '16d32.3', 'long': '95d41.6', 'altitude': '13d42.3',
                            'assumedLat': '-153d38.4', 'assumedLong': '74d35.3', 'correctedDistance': '-3950', 'correctedAzimuth': '164d43.1'})
