3ae7e41c21afac584385f3f37e01661b90a6d1ea
gsj0001 <gsj0001@tigermail.auburn.edu>
2017-04-27 13:52:47 -0500

    Red Light |

--- softwareprocess/correctTest.py
+++ softwareprocess/correctTest.py
@@ -6,7 +6,7 @@ class correctTest(unittest.TestCase):
     def test1000_MandatoryInformationMissing(self):
         self.assertEquals(SM.correct({'op':'correct'}), {'error':'mandatory information is missing', 'op':'correct'})
     def test1001_LatMissing(self):
-        self.assertEquals(SM.correct({'op':'correct', 'long':'95d41.6','altitude':'13d42.3','assumedLat':'-53d38.4','assumedLong':'74d35.3'},
+        self.assertEquals(SM.correct({'op':'correct', 'long':'95d41.6','altitude':'13d42.3','assumedLat':'-53d38.4','assumedLong':'74d35.3'}),
                                      {'error':'mandatory information is missing', 'op':'correct'}))
     def test1002_InvalidLat(self):
         self.assertEquals(SM.correct({'op':'correct', 'lat': '16.0d32.3', 'long':'95d41.6','altitude':'13d42.3','assumedLat':'-53d38.4','assumedLong':'74d35.3'}),
